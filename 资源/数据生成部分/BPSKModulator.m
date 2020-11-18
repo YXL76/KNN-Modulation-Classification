aa42d533e3267b1d08db60a3a1bc0cbf949d7070
YXL <chenxin.lan.76@gmail.com>
2020-11-18 12:56:46 +0800

    🌈style: format

--- 资源/数据生成部分/BPSKModulator.m
+++ 资源/数据生成部分/BPSKModulator.m
@@ -1,8 +1,10 @@
 function [out] = BPSKModulator(in)
-persistent Modulator   
-if isempty(Modulator)
-    Modulator=comm.BPSKModulator;
-    %Modulator=comm.PSKModulator(2,'BitInput',true);
-end
-out = Modulator(in);
+    persistent Modulator
+
+    if isempty(Modulator)
+        Modulator = comm.BPSKModulator;
+        %Modulator=comm.PSKModulator(2,'BitInput',true);
+    end
+
+    out = Modulator(in);
 end
--- 资源/数据生成部分/QPSKModulator.m
+++ 资源/数据生成部分/QPSKModulator.m
@@ -1,8 +1,10 @@
 function [out] = QPSKModulator(in)
-persistent Modulator   
-if isempty(Modulator)
-    Modulator=comm.QPSKModulator('BitInput',true);
-    %demodLLR=comm.QPSKDemodulator('BitOutput',true,'DecisionMethod','Log-likelihood ratio','VarianceSource','Input port');
-end
-out = Modulator(in);
+    persistent Modulator
+
+    if isempty(Modulator)
+        Modulator = comm.QPSKModulator('BitInput', true);
+        %demodLLR=comm.QPSKDemodulator('BitOutput',true,'DecisionMethod','Log-likelihood ratio','VarianceSource','Input port');
+    end
+
+    out = Modulator(in);
 end
