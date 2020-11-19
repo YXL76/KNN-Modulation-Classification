0e7555317728382a43e547fd0efe73b97a968d92
YXL <chenxin.lan.76@gmail.com>
2020-11-19 20:16:54 +0800

    🦄refactor: use flask8 to lint file

--- 资源/调制识别部分/fuc1.py
+++ 资源/调制识别部分/fuc1.py
@@ -1,8 +1,9 @@
-import numpy as np
+import collections
 import operator
 from collections import defaultdict
-import collections
+
 import matplotlib.pyplot as plt
+import numpy as np
 
 # KNN的具体实现
 
@@ -23,7 +24,8 @@ def classify0(inX, dataset, labels, k):
         votelabel = labels[sortedDistIndex[i]]
         classCount[votelabel] = classCount.get(votelabel, 0) + 1
     sortedClasscount = sorted(
-        classCount.items(), key=operator.itemgetter(1), reverse=True)
+        classCount.items(), key=operator.itemgetter(1), reverse=True
+    )
     return sortedClasscount[0][0]
 
 
@@ -36,7 +38,7 @@ def file2matrix(filename):
     index = 0
     for line in fr.readlines():
         line = line.strip()
-        listFromLine = line.split('\t')
+        listFromLine = line.split("\t")
         returnMat[index, :] = listFromLine[0:9]
         classLabelVector.append(float(listFromLine[-1]))
         index += 1
@@ -45,45 +47,54 @@ def file2matrix(filename):
 
 def BPSKModulationClassTest():
     sampleDataMat, sampleLabels = file2matrix(
-        'data/sample.dat')  # load data setfrom file
-    SNR = [2*x for x in range(-2, 6)]
+        "data/sample.dat"
+    )  # load data setfrom file
+    SNR = [2 * x for x in range(-2, 6)]
     accuracy = defaultdict(list)
     correctCount = defaultdict(list)
     classifierResult = defaultdict(list)
     for snr in SNR:
         testDataMat, testLabels = file2matrix(
-            'data/testBPSK-'+str(snr)+'.dat')  # load data setfrom file
+            "data/testBPSK-" + str(snr) + ".dat"
+        )  # load data setfrom file
         numTestVecs = testDataMat.shape[0]
         correctCount[snr] = 0.0
         for i in range(numTestVecs):
             classifierResult[snr] = classify0(
-                testDataMat[i, :], sampleDataMat[:, :], sampleLabels[:], 10)
-            #print ("the classifier came back with: %d, the real answer is: 2" % (classifierResult[snr]))
-            if (classifierResult[snr] == 2):
+                testDataMat[i, :], sampleDataMat[:, :], sampleLabels[:], 10
+            )
+            """
+            print(
+                "the classifier came back with: %d, the real answer is: 2"
+                % (classifierResult[snr])
+            )
+            """
+            if classifierResult[snr] == 2:
                 correctCount[snr] += 1.0
-        accuracy[snr] = correctCount[snr]/(numTestVecs)
-        print("the total correct rate on %d dB SNR is:" %
-              (snr), (accuracy[snr]))
+        accuracy[snr] = correctCount[snr] / (numTestVecs)
+        print(
+            "the total correct rate on %d dB SNR is:" % (snr), (accuracy[snr])
+        )
         print(correctCount[snr])
     accuracy = collections.OrderedDict(
-        sorted(accuracy.items()))  # sort by ascending SNR values
+        sorted(accuracy.items())
+    )  # sort by ascending SNR values
 
-# 画图
+    # 画图
     print(accuracy)
-    plt.style.use('classic')
     plt.figure(figsize=(8, 5), dpi=100)
     x = SNR
     y = list(accuracy.values())
-    plt.plot(x, y, marker="o", linewidth=2.0,
-             linestyle='dashed', color='royalblue')
+    plt.plot(
+        x, y, marker="o", linewidth=2.0, linestyle="dashed", color="royalblue"
+    )
     plt.axis([0, 10, 0, 1])
-    plt.xticks(np.arange(min(x), max(x)+1, 2.0))
+    plt.xticks(np.arange(min(x), max(x) + 1, 2.0))
     plt.yticks(np.arange(0, 1, 0.10))
 
-    ttl = plt.title('SNR vs Accuracy - BPSK', fontsize=16)
-    ttl.set_weight('bold')
-    plt.xlabel('SNR (dB)', fontsize=14)
-    plt.ylabel('Test accuracy', fontsize=14)
+    plt.title("SNR vs Accuracy - BPSK", fontsize=16)
+    plt.xlabel("SNR (dB)", fontsize=14)
+    plt.ylabel("Test accuracy", fontsize=14)
     plt.grid()
 
     plt.show()
@@ -91,44 +102,51 @@ def BPSKModulationClassTest():
 
 def QPSKModulationClassTest():
     sampleDataMat, sampleLabels = file2matrix(
-        'data/sample.dat')  # load data setfrom file
-    SNR = [2*x for x in range(-2, 6)]
+        "data/sample.dat"
+    )  # load data setfrom file
+    SNR = [2 * x for x in range(-2, 6)]
     accuracy = defaultdict(list)
     correctCount = defaultdict(list)
     classifierResult = defaultdict(list)
     for snr in SNR:
         testDataMat, testLabels = file2matrix(
-            'data/testQPSK-'+str(snr)+'.dat')  # load data setfrom file
+            "data/testQPSK-" + str(snr) + ".dat"
+        )  # load data setfrom file
         numTestVecs = testDataMat.shape[0]
         correctCount[snr] = 0.0
         for i in range(numTestVecs):
             classifierResult[snr] = classify0(
-                testDataMat[i, :], sampleDataMat[:, :], sampleLabels[:], 10)
-            #print ("the classifier came back with: %d, the real answer is: 4" % (classifierResult[snr]))
-            if (classifierResult[snr] == 4):
+                testDataMat[i, :], sampleDataMat[:, :], sampleLabels[:], 10
+            )
+            """
+            print("the classifier came back with: %d, the real answer is: 4" %
+                  (classifierResult[snr]))
+            """
+            if classifierResult[snr] == 4:
                 correctCount[snr] += 1.0
-        accuracy[snr] = correctCount[snr]/(numTestVecs)
-        print("the total correct rate on %d dB SNR is:" %
-              (snr), (accuracy[snr]))
+        accuracy[snr] = correctCount[snr] / (numTestVecs)
+        print(
+            "the total correct rate on %d dB SNR is:" % (snr), (accuracy[snr])
+        )
         print(correctCount[snr])
     accuracy = collections.OrderedDict(
-        sorted(accuracy.items()))  # sort by ascending SNR values
+        sorted(accuracy.items())
+    )  # sort by ascending SNR values
 
     print(accuracy)
-    plt.style.use('classic')
     plt.figure(figsize=(8, 5), dpi=100)
     x = SNR
     y = list(accuracy.values())
-    plt.plot(x, y, marker="o", linewidth=2.0,
-             linestyle='dashed', color='#800080')
+    plt.plot(
+        x, y, marker="o", linewidth=2.0, linestyle="dashed", color="#800080"
+    )
     plt.axis([0, 10, 0, 1])
-    plt.xticks(np.arange(min(x), max(x)+1, 2.0))
+    plt.xticks(np.arange(min(x), max(x) + 1, 2.0))
     plt.yticks(np.arange(0, 1, 0.10))
 
-    ttl = plt.title('SNR vs Accuracy - QPSK', fontsize=16)
-    ttl.set_weight('bold')
-    plt.xlabel('SNR (dB)', fontsize=14)
-    plt.ylabel('Test accuracy', fontsize=14)
+    plt.title("SNR vs Accuracy - QPSK", fontsize=16)
+    plt.xlabel("SNR (dB)", fontsize=14)
+    plt.ylabel("Test accuracy", fontsize=14)
     plt.grid()
 
     plt.show()
