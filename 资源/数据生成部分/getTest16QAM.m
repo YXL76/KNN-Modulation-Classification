1929ed2907a9ea3748f822d9dff4a5358cb69b0c
YXL <chenxin.lan.76@gmail.com>
2020-11-19 23:53:53 +0800

    ✨feat: use multi N

--- 资源/数据生成部分/getTest16QAM.m
+++ 资源/数据生成部分/getTest16QAM.m
@@ -3,48 +3,54 @@ clear;
 clc;
 
 rows = 1000;
-cols = 500 * 4;
 NFeatures = 5;
 test_data = zeros(rows, NFeatures);
 SNR = (0:5:40);
+N = [200 500];
 
-for i = 1:length(SNR)
+for j = 1:length(N)
 
-    %% Simulation parameters
-    M = 16;
-    snr = SNR(i);
+    cols = N(j) * 4;
 
-    %% Simulate
-    %16QAM
-    % Generate random data
-    data = randi([0 1], rows, cols);
+    for i = 1:length(SNR)
 
-    % QAM 映射
-    [data1] = data';
-    data_cell = mat2cell(data1, cols, ones(1, rows));
-    signalData = cellfun(@(x) qammod(x, M, 'InputType', 'bit', 'UnitAveragePower', true), data_cell, 'UniformOutput', false);
-    signalDataMat = cell2mat(signalData);
-    dataMod = mat2cell(signalDataMat', ones(1, rows), cols / 4);
+        %% Simulation parameters
+        M = 16;
+        snr = SNR(i);
 
-    % AWGN 加噪
-    dataRx = cellfun(@(x) awgn(x, snr), dataMod, 'UniformOutput', false);
+        %% Simulate
+        %16QAM
+        % Generate random data
+        data = randi([0 1], rows, cols);
 
-    % 获取高阶累积量
-    cumulants = cellfun(@(x) func_get_cumulants(x), dataRx, 'UniformOutput', false);
-    cumulantsMat = cell2mat(cumulants);
+        % QAM 映射
+        [data1] = data';
+        data_cell = mat2cell(data1, cols, ones(1, rows));
+        signalData = cellfun(@(x) qammod(x, M, 'InputType', 'bit', 'UnitAveragePower', true), data_cell, 'UniformOutput', false);
+        signalDataMat = cell2mat(signalData);
+        dataMod = mat2cell(signalDataMat', ones(1, rows), cols / 4);
 
-    for row = 1:rows
+        % AWGN 加噪
+        dataRx = cellfun(@(x) awgn(x, snr), dataMod, 'UniformOutput', false);
 
-        idx = 1;
+        % 获取高阶累积量
+        cumulants = cellfun(@(x) func_get_cumulants(x), dataRx, 'UniformOutput', false);
+        cumulantsMat = cell2mat(cumulants);
 
-        while idx <= NFeatures
-            test_data(row, idx) = cumulantsMat(row, idx);
-            idx = idx + 1;
+        for row = 1:rows
+
+            idx = 1;
+
+            while idx <= NFeatures
+                test_data(row, idx) = cumulantsMat(row, idx);
+                idx = idx + 1;
+            end
+
         end
 
+        %% save
+        filename = [fullfile('digits', 'test16QAM-'), num2str(N(j)), '-', num2str(snr), '.dat'];
+        dlmwrite(filename, test_data, 'delimiter', '\t', 'newline', 'pc');
     end
 
-    %% save
-    filename = [fullfile('digits', 'test16QAM-'), num2str(snr), '.dat'];
-    dlmwrite(filename, test_data, 'delimiter', '\t', 'newline', 'pc');
 end
--- 资源/数据生成部分/getTestQPSK.m
+++ 资源/数据生成部分/getTestQPSK.m
@@ -3,49 +3,55 @@ clear;
 clc;
 
 rows = 1000;
-cols = 500 * 2;
 NFeatures = 5;
 test_data = zeros(rows, NFeatures);
 SNR = (-4:2:10);
+N = [200 500];
 
-for i = 1:length(SNR)
+for j = 1:length(N)
 
-    %% Simulation parameters
-    M = 4;
-    snr = SNR(i);
+    cols = N(j) * 2;
 
-    %% Simulate
-    %QPSK
-    % Generate random data
-    data = randi([0 1], rows, cols);
+    for i = 1:length(SNR)
 
-    % PSK 映射
-    [data1] = data';
-    data_cell = mat2cell(data1, cols, ones(1, rows));
-    signalData = cellfun(@(x) QPSKModulator(x), data_cell, 'UniformOutput', false);
-    %signalData = cellfun(@(x) qammod(x, M, 'InputType', 'bit', 'UnitAveragePower', true), data_cell, 'UniformOutput',false);
-    signalDataMat = cell2mat(signalData);
-    dataMod = mat2cell(signalDataMat', ones(1, rows), cols / 2);
+        %% Simulation parameters
+        M = 4;
+        snr = SNR(i);
 
-    % AWGN 加噪
-    dataRx = cellfun(@(x) awgn(x, snr), dataMod, 'UniformOutput', false);
+        %% Simulate
+        %QPSK
+        % Generate random data
+        data = randi([0 1], rows, cols);
 
-    % 获取高阶累积量
-    cumulants = cellfun(@(x) func_get_cumulants(x), dataRx, 'UniformOutput', false);
-    cumulantsMat = cell2mat(cumulants);
+        % PSK 映射
+        [data1] = data';
+        data_cell = mat2cell(data1, cols, ones(1, rows));
+        signalData = cellfun(@(x) QPSKModulator(x), data_cell, 'UniformOutput', false);
+        %signalData = cellfun(@(x) qammod(x, M, 'InputType', 'bit', 'UnitAveragePower', true), data_cell, 'UniformOutput',false);
+        signalDataMat = cell2mat(signalData);
+        dataMod = mat2cell(signalDataMat', ones(1, rows), cols / 2);
 
-    for row = 1:rows
+        % AWGN 加噪
+        dataRx = cellfun(@(x) awgn(x, snr), dataMod, 'UniformOutput', false);
 
-        idx = 1;
+        % 获取高阶累积量
+        cumulants = cellfun(@(x) func_get_cumulants(x), dataRx, 'UniformOutput', false);
+        cumulantsMat = cell2mat(cumulants);
 
-        while idx <= NFeatures
-            test_data(row, idx) = cumulantsMat(row, idx);
-            idx = idx + 1;
+        for row = 1:rows
+
+            idx = 1;
+
+            while idx <= NFeatures
+                test_data(row, idx) = cumulantsMat(row, idx);
+                idx = idx + 1;
+            end
+
         end
 
+        %% save
+        filename = [fullfile('digits', 'testQPSK-'), num2str(N(j)), '-', num2str(snr), '.dat'];
+        dlmwrite(filename, test_data, 'delimiter', '\t', 'newline', 'pc');
     end
 
-    %% save
-    filename = [fullfile('digits', 'testQPSK-'), num2str(snr), '.dat'];
-    dlmwrite(filename, test_data, 'delimiter', '\t', 'newline', 'pc');
 end
